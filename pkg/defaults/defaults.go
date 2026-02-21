05bbaa017c2c7243560e6af310d9681a251499ec
WAFtester <dev@waftester.com>
2026-02-21 16:22:39 +0400

    chore(cli): bump version to 2.9.24

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -18,7 +18,7 @@ import (
 )
 
 // Version is the current WAFtester version
-const Version = "2.9.23"
+const Version = "2.9.24"
 
 // ToolName is the canonical tool name for output formats and integrations
 const ToolName = "waftester"
